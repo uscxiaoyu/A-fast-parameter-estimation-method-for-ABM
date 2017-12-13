3ce6750acbbd2021e7ef5d0ade99c8903ed379a7
uscxiaoyu <jxndtbxiaoyu@163.com>
2017-12-13 12:35:31 +0800

    update

--- Estimate_ABM.py
+++ Estimate_ABM.py
@@ -12,40 +12,36 @@ pl.rcParams.update({'font.size': 12, 'font.family': 'STIXGeneral', 'mathtext.fon
 
 class Diffuse: #默认网络结构为节点数量为10000，边为60000的单向随机网络
     repetes = 10 #默认多次扩散重复次数
-    def __init__(self,p,q,num_runs,G=nx.gnm_random_graph(10000,30000)):
+    def __init__(self, p, q, num_runs, G=nx.gnm_random_graph(10000, 30000)):
+        if not nx.is_directed(G):
+            self.G = G.to_directed()
+        else:
+            self.G = G
         self.p = p
         self.q = q
-        self.G = G.to_directed()
         self.num_runs = num_runs
         
-    def decision(self,i): #个体状态决策规则
-        influ = len([k for k in self.DG[i].get('prede',[]) if self.DG[k]['state'] == 1])           
-        prob = self.p + self.q*influ
-        if random.random() <= prob:
-            return True
-        else:
-            return False
+    def decision(self, i): #个体状态决策规则
+        influ = sum([1 for k in self.G.predecessors(i) if self.G[k]['state']])           
+        prob = self.p + self.q * influ
+        return True if random.random() <= prob else False
         
     def single_diffuse(self): #单次扩散
-        self.DG = dc(self.G) #取网络的深层copy，以使原网络不被“污染” 
-        for i in self.DG.nodes():
-            self.DG[i]['prede'] = self.DG.predecessors(i)
-            self.DG[i]['state'] = 0
+        for i in self.G.nodes():
+            self.G[i]['state'] = False
 
-        non_adopt_set = [i for i in self.DG.nodes() if self.DG[i]['state'] == 0]
+        non_adopt_set = [i for i in self.G]
         num_of_adopt = []
 
-        j = 1
-        while j <= self.num_runs:                                 
+        for j in range(self.num_runs):                                 
             x = 0
             random.shuffle(non_adopt_set)
             for i in non_adopt_set:                   
                 if self.decision(i):
-                    self.DG[i]['state'] = 1
+                    self.G[i]['state'] = 1
                     non_adopt_set.remove(i)
-                    x = x+1
+                    x += 1
             num_of_adopt.append(x)
-            j = j+1
 
         return num_of_adopt
     
@@ -57,15 +53,15 @@ class Diffuse: #默认网络结构为节点数量为10000，边为60000的单向
         return adopt_cont
         
 class Random_Grid_Search:
-    t_n = 500 #抽样量
-    c_n = 50 #保留参数量
-    threshold = 1e-6 #循环停止阈值
-    orig_points = [] #初始化边界点
-    def __init__(self,s): #初始化实例参数
-        self.s = np.array(s)  #待拟合曲线
+    t_n = 500  # 抽样量
+    c_n = 50  # 保留参数量
+    threshold = 1e-6  # 循环停止阈值
+    orig_points = []  # 初始化边界点
+    def __init__(self,s):  # 初始化实例参数
+        self.s = np.array(s)  # 待拟合曲线
         self.s_len = len(s)
-        self.para_range = [[1e-6,0.1],[1e-3,1],[sum(s),4*sum(s)]]  #参数范围
-        self.p_range = [[1e-6,0.1],[1e-3,1],[sum(s),4*sum(s)]]  #用于产生边界节点的参数范围 
+        self.para_range = [[1e-6, 0.1], [1e-3, 1],[sum(s), 4 * sum(s)]]  # 参数范围
+        self.p_range = [[1e-6, 0.1],[1e-3, 1],[sum(s), 4 * sum(s)]]  # 用于产生边界节点的参数范围 
     
     def gener_orig(self): #递归产生边界点
         if len(self.p_range) == 0:
@@ -73,13 +69,13 @@ class Random_Grid_Search:
         else:  
             pa = self.p_range[-1]
             if self.orig_points == []:
-                self.orig_points = [[pa[0]],[pa[1]]]  #初始化,排除orig_points为空的情形
+                self.orig_points = [[pa[0]], [pa[1]]]  # 初始化,排除orig_points为空的情形
             else:
-                self.orig_points = [[pa[0]]+x for x in self.orig_points]+[[pa[1]]+x for x in self.orig_points]  #二分裂
+                self.orig_points = [[pa[0]] + x for x in self.orig_points] + [[pa[1]]+x for x in self.orig_points]  # 二分裂
             self.p_range.pop()
             return self.gener_orig()
     
-    def sample(self,c_range): #抽样参数点
+    def sample(self, c_range):  # 抽样参数点
         p_list = []
         for pa in c_range:
             if isinstance(pa[0],float):
@@ -91,59 +87,58 @@ class Random_Grid_Search:
         p_list = np.array(p_list).T
         return p_list.tolist()
     
-    def f(self,params): #扩散函数
+    def f(self, params): #扩散函数
         diffu_cont = np.zeros(self.s_len)
         t_list = np.arange(1,self.s_len+1)
-        a = np.array([1 - np.exp(-(params[0]+params[1])*t) for t in t_list])
-        b = np.array([1 + params[1]/params[0]*np.exp(-(params[0]+params[1])*t) for t in t_list])
-        diffu_cont = params[2]*a/b
+        a = np.array([1 - np.exp(-(params[0] + params[1]) * t) for t in t_list])
+        b = np.array([1 + params[1] / params[0] * np.exp(-(params[0] + params[1]) * t) for t in t_list])
+        diffu_cont = params[2] * a / b
 
         adopt_cont = np.zeros_like(diffu_cont)
         adopt_cont[0] = diffu_cont[0]
-        for t in xrange(1,self.s_len):
+        for t in range(1,self.s_len):
             adopt_cont[t] = diffu_cont[t] - diffu_cont[t-1]
 
         return adopt_cont
     
-    def r2(self,params):
+    def r2(self, params):
         f_act = self.f(params)
-        tse = np.sum(np.square(self.s-f_act))
+        tse = np.sum(np.square(self.s - f_act))
         mean_y = np.mean(self.s)
-        ssl = np.sum(np.square(self.s-mean_y))
-        R_2 = (ssl-tse)/ssl
+        ssl = np.sum(np.square(self.s - mean_y))
+        R_2 = (ssl - tse) / ssl
         return R_2
 
     def mse(self,params):  #定义适应度函数（mse）
         a = self.f(params)
-        sse = np.sum(np.square(self.s-a))
-        return np.sqrt(sse)/self.s_len #均方误
+        sse = np.sum(np.square(self.s - a))
+        return np.sqrt(sse) / self.s_len  # 均方误
     
     def optima_search(self):
         self.gener_orig()
         c_range = dc(self.para_range)
         samp = self.sample(c_range)
-        solution = sorted([self.mse(x)]+x for x in samp+self.orig_points)[:self.c_n]
-        u = 1
-        while 1:
-            params_min = np.min(np.array(solution),0) #最小值
-            params_max = np.max(np.array(solution),0) #最大值
-            c_range = [[params_min[j+1],params_max[j+1]] for j in range(len(c_range))] #重新定界
+        solution = sorted([self.mse(x)] + x for x in samp + self.orig_points)[:self.c_n]
+        for u in range(100):
+            params_min = np.min(solution,0)[1:]  # 最小值
+            params_max = np.max(solution,0)[1:]  # 最大值
+            c_range = [[params_min[j], params_max[j]] for j in range(params_min.size)]  # 重新定界
             samp = self.sample(c_range)
-            solution = sorted([[self.mse(x)]+x for x in samp]+solution)[:self.c_n]
+            solution = sorted([[self.mse(x)] + x for x in samp] + solution)[:self.c_n]
             r = sorted([x[0] for x in solution])
-            v = (r[-1]-r[0])/r[0]
+            v = (r[-1] - r[0]) / r[0]
             if v < self.threshold:        
                 break
-            if u > 100:
-                print 'Searching ends in 100 runs'
-                break
-            u+=1
+        else:
+            print 'Searching ends in 100 runs'
+            break
+
         return solution[0]  #sse,p,q,m
         
 class Estimate_ABM:
-    num_conds = 2 #构建网格的节点个数
+    num_conds = 2  # 构建网格的节点个数
     k = 6
-    def __init__(self, s, intv_p = 0.001,intv_q = 0.005,G=nx.gnm_random_graph(10000,30000)):
+    def __init__(self, s, intv_p=0.001, intv_q = 0.005, G=nx.gnm_random_graph(10000, 30000)):
         self.s = s
         self.s_len = len(s)
         self.intv_p = 0.001
@@ -152,38 +147,38 @@ class Estimate_ABM:
     
     def r2(self,f_act):
         f_act = np.array(f_act)
-        tse = np.sum(np.square(self.s-f_act))
+        tse = np.sum(np.square(self.s - f_act))
         mean_y = np.mean(self.s)
-        ssl = np.sum(np.square(self.s-mean_y))
-        R_2 = (ssl-tse)/ssl
+        ssl = np.sum(np.square(self.s - mean_y))
+        R_2 = (ssl - tse) / ssl
         return R_2
     
-    def get_M(self,p,q):  #获取对应扩散率曲线的最优潜在市场容量
-        diffu = Diffuse(p,q,self.s_len,G=self.G)
+    def get_M(self,p,q):  # 获取对应扩散率曲线的最优潜在市场容量
+        diffu = Diffuse(p, q, self.s_len, G=self.G)
         s_estim =diffu.repete_diffuse()
-        x = np.mean(s_estim,axis=0)
-        a = np.sum(np.square(x))/np.sum(self.s) #除以np.sum(self.s)是为减少a的大小
-        b = -2*np.sum(x*self.s)/np.sum(self.s)
-        c = np.sum(np.square(self.s))/np.sum(self.s)
-        mse,sigma = np.sqrt(sum(self.s)*(4*a*c-b**2)/(4*a*self.s_len)),-b/(2*a)
-        m = sigma*self.G.number_of_nodes()
+        x = np.mean(s_estim, axis=0)
+        a = np.sum(np.square(x)) / np.sum(self.s)  # 除以np.sum(self.s)是为减少a的大小
+        b = -2 * np.sum(x * self.s) / np.sum(self.s)
+        c = np.sum(np.square(self.s)) / np.sum(self.s)
+        mse, sigma = np.sqrt(sum(self.s) * (4 * a * c - b ** 2) / (4 * a * self.s_len)), -b / (2 * a)
+        m = sigma * self.G.number_of_nodes()
         return mse,p,q,m,x*sigma
     
-    def gener_p0_q0(self): # 生成初始搜索点(p0,q0)
+    def gener_p0_q0(self):  # 生成初始搜索点(p0,q0)
         rgs = Random_Grid_Search(self.s)
-        P0,Q0 = rgs.optima_search()[1:3] #SABM最优点（P0,Q0）
-        p_range = np.linspace(0.4*P0,P0,num=3)
-        q_range = np.linspace(0.2*Q0/self.k,0.6*Q0/self.k,num=3)
+        P0,Q0 = rgs.optima_search()[1:3]  # SABM最优点（P0,Q0）
+        p_range = np.linspace(0.4 * P0, P0, num=3)
+        q_range = np.linspace(0.2 * Q0 / self.k, 0.6 * Q0 / self.k, num=3)
         to_fit = {}    
         params_cont = []
         for p in p_range:
             for q in q_range:
-                diffu = Diffuse(p,q,self.s_len,self.G)
+                diffu = Diffuse(p, q, self. s_len, self.G)
                 s_estim = diffu.repete_diffuse()
                 s_estim_avr = np.mean(s_estim,axis=0)
                 rgs_1 = Random_Grid_Search(s_estim_avr)
-                P,Q = rgs_1.optima_search()[1:3]
-                params_cont.append([[p,q],[P,Q]])
+                P, Q = rgs_1.optima_search()[1:3]
+                params_cont.append([[p, q], [P, Q]])
         
         to_fit['p'] = [x[0][0] for x in params_cont]
         to_fit['q'] = [x[0][1] for x in params_cont]
@@ -191,25 +186,25 @@ class Estimate_ABM:
         to_fit['Q'] = [x[1][1] for x in params_cont]
         to_fit = pd.DataFrame(to_fit)
 
-        result_p = smf.ols('p~P+Q-1',data=to_fit).fit()
-        result_q = smf.ols('q~P+Q-1',data=to_fit).fit()
+        result_p = smf.ols('p~P+Q-1', data=to_fit).fit()
+        result_q = smf.ols('q~P+Q-1', data=to_fit).fit()
 
-        p0 = result_p.params['P']*P0 + result_p.params['Q']*Q0
-        q0 = result_q.params['P']*P0 + result_q.params['Q']*Q0
-        return p0,q0
+        p0 = result_p.params['P'] * P0 + result_p.params['Q'] * Q0
+        q0 = result_q.params['P'] * P0 + result_q.params['Q'] * Q0
+        return p0, q0
     
-    def solution_search(self,p0,q0):
+    def solution_search(self, p0, q0):
         solution_cont = []
         diff_cont = []
         pq_cont = []
         
-        for p in (p0-self.intv_p, p0, p0+self.intv_p):
-            for q in (q0-self.intv_q, q0, q0+self.intv_q):
-                p,q = round(p,4),round(q,4)  #防止出错
-                pq_cont.append((p,q))
-                solution = self.get_M(p,q)
-                solution_cont.append(solution[:4]) #M_sse,p,q,s_M
-                diff_cont.append(solution[4]) #x
+        for p in (p0 - self.intv_p, p0, p0 + self.intv_p):
+            for q in (q0 - self.intv_q, q0, q0 + self.intv_q):
+                p,q = round(p, 4), round(q, 4)  # 防止出错
+                pq_cont.append((p, q))
+                solution = self.get_M(p, q)
+                solution_cont.append(solution[:4]) # M_sse,p,q,s_M
+                diff_cont.append(solution[4])  # x
 
         best_solution = sorted(solution_cont)[:self.num_conds] 
         while 1:
